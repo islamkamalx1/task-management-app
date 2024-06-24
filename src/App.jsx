f3e31ca0d663a580409cf23531719c30551d6248
Islam Kamal <islamkamalx1>
2024-06-25 02:12:58 +0300

    Revert design | search | add task | remove task
    This reverts commit 41f7c4d09c35d7072096a7b50b3181dc18351ce8.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import TaskCards from "./components/TaskCards.jsx";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="container mx-auto">
-      <h1 className="text-orange text-2xl font-bold text-center py-4">
-        Task Management App
-      </h1>
-      <p className="text-yellow text-lg text-center ">
-        Arrange your tasks and be more productive today🎯
-      </p>
+  const [count, setCount] = useState(0)
 
-      <TaskCards />
-    </div>
-  );
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
